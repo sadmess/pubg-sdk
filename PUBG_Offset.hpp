438d426edc38a3ad3731e05ab899d5a5a88ba45f
sadmess <zhaojihao0627@163.com>
2022-05-01 02:54:11 +0800

    17.1.6.7

---
+++ PUBG_Offset.hpp
@@ -0,0 +1,52 @@
+// AActor	Type:class FNetTag*
+RootComponent	=	0x1C0,//(0x8)
+// USceneComponent	Type:struct FbReplicatesAttachmentReference
+RelativeRotation	=	0x250,//(0xC)
+// USceneComponent	Type:struct FAttachChildren
+ComponentVelocity	=	0x2AC,//(0xC)
+// USkeletalMeshComponent	Type:class FSubInstances*
+AnimScriptInstance	=	0xCA0,//(0x8)
+// UCharacterMovementComponent	Type:float
+MaxAcceleration	=	0x318,//(0x4)
+// UCharacterMovementComponent	Type:struct FLastUpdateLocation
+Acceleration	=	0x3B8,//(0xC)
+// UCharacterMovementComponent	Type:struct FServerLastTransformUpdateTimeStamp
+LastUpdateVelocity	=	0x3E0,//(0xC)
+// ACharacter	Type:class FReplicatedBasedMovement*
+Mesh	=	0x470,//(0x8)
+// AController	Type:class FbAttachToPawn*
+Pawn	=	0x410,//(0x8)
+// APlayerController	Type:class FPlayerCameraManagerClass*
+PlayerCameraManager	=	0x4B8,//(0x8)
+// APlayerCameraManager	Type:struct FViewPitchMax
+CameraCache	=	0x1640,//(0x5D0)
+// Class:FMinimalViewInfo	Type:struct FAspectRatio
+Location	=	0x0,//(0xC)
+// Class:FMinimalViewInfo	Type:struct FOffCenterProjectionOffset
+Rotation	=	0x590,//(0xC)
+// Class:FMinimalViewInfo	Type:float
+FOV	=	0x5AC,//(0x4)
+// ATslCharacter	Type:int
+SpectatedCount	=	0x1080,//(0x4)
+// ATslCharacter	Type:struct FString
+CharacterName	=	0x1090,//(0x10)
+// ATslCharacter	Type:float
+GroggyHealth	=	0x1410,//(0x4)
+// ATslCharacter	Type:class FBanks*
+WeaponProcessor	=	0x1468,//(0x8)
+// ATslCharacter	Type:int
+LastTeamNum	=	0x14B0,//(0x4)
+// ATslCharacter	Type:float
+Health	=	0x27FC,//(0x4)
+// UWeaponTrajectoryData	Type:struct FTrajectoryConfig
+RecoilConfig	=	0x48,//(0xD0)
+// UWeaponTrajectoryData	Type:struct FConsoleWeaponRecoilData
+TrajectoryConfig	=	0x118,//(0x48)
+// ATslWeapon_Trajectory	Type:class FbSpawnBulletFromBarrel*
+WeaponTrajectoryData	=	0x1000,//(0x8)
+// UWeaponProcessorComponent	Type:TArray<TArray<struct FWeaponArmInfo_OwnerOnly>>
+EquippedWeapons	=	0x2C8,//(0x10)
+// UWeaponProcessorComponent	Type:struct FWeaponArmInfo_OwnerOnly
+WeaponArmInfo	=	0x2E8,//(0x5)
+// UCrowdFollowingComponent	Type:class FAvoidanceGroup*
+CharacterMovement	=	0x4F0,//(0x8)
