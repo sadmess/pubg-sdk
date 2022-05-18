c4a29b97721d9ea966a89d3d9fd360a6e17d634c
sadmess <zhaojihao0627@163.com>
2022-05-18 21:49:11 +0800

    new

--- 17.2.5.7/PUBG_Offset.hpp
+++ 17.2.5.7/PUBG_Offset.hpp
@@ -1,30 +1,30 @@
-// AActor	Type:class FOnInputTouchEnd*
+// AActor	Type:class F*
 RootComponent	=	0x230,//(0x8)
-// USceneComponent	Type:struct FClientAttachedChildren
+// USceneComponent	Type:struct F
 ComponentVelocity	=	0x230,//(0xC)
-// USceneComponent	Type:struct FDetailMode
+// USceneComponent	Type:struct F
 RelativeRotation	=	0x338,//(0xC)
-// USkeletalMeshComponent	Type:class FSubInstances*
+// USkeletalMeshComponent	Type:class F*
 AnimScriptInstance	=	0xC90,//(0x8)
 // UCharacterMovementComponent	Type:float
 MaxAcceleration	=	0x308,//(0x4)
-// UCharacterMovementComponent	Type:struct FLastUpdateLocation
+// UCharacterMovementComponent	Type:struct F
 Acceleration	=	0x3A8,//(0xC)
-// UCharacterMovementComponent	Type:struct FServerLastTransformUpdateTimeStamp
+// UCharacterMovementComponent	Type:struct F
 LastUpdateVelocity	=	0x3D0,//(0xC)
-// ACharacter	Type:class FJumpMaxHoldTime*
+// ACharacter	Type:class F*
 Mesh	=	0x4B0,//(0x8)
-// AController	Type:class FPlayerState*
+// AController	Type:class F*
 Pawn	=	0x430,//(0x8)
-// APlayerController	Type:class FPlayerCameraManagerClass*
+// APlayerController	Type:class F*
 PlayerCameraManager	=	0x4A0,//(0x8)
-// APlayerCameraManager	Type:struct FDefaultOrthoWidth
+// APlayerCameraManager	Type:struct F
 CameraCache	=	0x410,//(0x5D0)
-// Class:FMinimalViewInfo	Type:struct FbConstrainAspectRatio
+// Class:FMinimalViewInfo	Type:struct F
 Rotation	=	0x0,//(0xC)
 // Class:FMinimalViewInfo	Type:float
 FOV	=	0x1C,//(0x4)
-// Class:FMinimalViewInfo	Type:struct FPostProcessBlendWeight
+// Class:FMinimalViewInfo	Type:struct F
 Location	=	0x20,//(0xC)
 // ATslCharacter	Type:struct FString
 CharacterName	=	0x12B0,//(0x10)
@@ -34,19 +34,17 @@ GroggyHealth	=	0x12F8,//(0x4)
 SpectatedCount	=	0x1978,//(0x4)
 // ATslCharacter	Type:int
 LastTeamNum	=	0x19E0,//(0x4)
-// ATslCharacter	Type:class FUnarmedDamageType*
+// ATslCharacter	Type:class F*
 WeaponProcessor	=	0x1BD8,//(0x8)
 // ATslCharacter	Type:float
 Health	=	0x2198,//(0x4)
-// UWeaponTrajectoryData	Type:struct FTrajectoryConfig
+// UWeaponTrajectoryData	Type:struct F
 RecoilConfig	=	0x40,//(0xD0)
-// UWeaponTrajectoryData	Type:struct FConsoleWeaponRecoilData
+// UWeaponTrajectoryData	Type:struct F
 TrajectoryConfig	=	0x110,//(0x48)
-// ATslWeapon_Trajectory	Type:class FbSpawnBulletFromBarrel*
+// ATslWeapon_Trajectory	Type:class F*
 WeaponTrajectoryData	=	0xFF8,//(0x8)
-// UWeaponProcessorComponent	Type:TArray<TArray<struct FWeaponArmInfo_OwnerOnly>>
-EquippedWeapons	=	0x2B8,//(0x10)
-// UWeaponProcessorComponent	Type:struct FWeaponArmInfo_OwnerOnly
+// UWeaponProcessorComponent	Type:struct F
 WeaponArmInfo	=	0x2D8,//(0x5)
-// UCrowdFollowingComponent	Type:class FAvoidanceGroup*
+// UCrowdFollowingComponent	Type:class F*
 CharacterMovement	=	0x4E0,//(0x8)
